c80fca328eccb9e15b9c7d544fd2c9b006b583e2
Ismael Kelm <kelmismael@gmail.com>
2026-04-17 21:28:31 -0300

    web

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,15 +1,10 @@
-function App() {
-  return (
-    <div>
-      <h1>Proyecto Seminario 2026 🚀</h1>
-    </div>
-  );
-}
+
 
 import AppRoutes from "./routes";
 
 function App() {
   return <AppRoutes />;
+
 }
 
 export default App;
---
+++ frontend-web/src/pages/Home/Home.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return <h1>Hola Bienvedido a mi pagina web de seminario 2026
+    
+  </h1>;
+}
+
