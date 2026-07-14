e7dd4dfb7e7acc6e0405b92d6ac138cadf52bb69
Ismael Kelm <kelmismael@gmail.com>
2026-07-13 21:00:31 -0300

    html css

--- frontend/src/components/home/LocationSection.tsx
+++ frontend/src/components/home/LocationSection.tsx
@@ -11,18 +11,18 @@ export default function LocationSection() {
         </h2>
 
         <p>
-          Av. Corrientes 1234
-          Buenos Aires, Argentina
+          Av. Lopez y Planes 3072,
+          Posadas - Misiones, Argentina
         </p>
 
         <p>
           Lunes a Viernes:
-          09:00 - 20:00
+          09:00 - 21:00
         </p>
 
         <p>
           Sábados:
-          09:00 - 18:00
+          09:00 - 21:00
         </p>
 
         <p>
@@ -34,15 +34,21 @@ export default function LocationSection() {
 
 
       <a
-        href="https://maps.google.com"
+        href="https://maps.app.goo.gl/zCRQFw6q2PJSby7F9"
         target="_blank"
         rel="noreferrer"
         className="location-map"
       >
 
-        <img
-          src="https://images.unsplash.com/photo-1524661135-423995f22d0b"
-          alt="mapa"
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d372.42043846859207!2d-55.90808840734604!3d-27.373208072130836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9457bf928031641b%3A0xcf967f8a9bbfa613!2sDavid%20Mart%C3%ADnez-Estilista%20Unisex!5e0!3m2!1ses-419!2sar!4v1778883672086!5m2!1ses-419!2sar"
+          width="400"
+          height="300"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Mapa de ubicación"
         />
 
       </a>
